f815cbcd18f6ac07b203bc44cc14f1da489928bd
Jordan Ballesteros <pookele@gmail.com>
2019-03-08 08:35:17 -1000

    Initial Commit

---
+++ AssignmentSix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AssignmentSix
+//
+//  Created by Amber Ballesteros on 3/8/19.
+//  Copyright © 2019 Jordan Ballesteros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
